3404ad4a5f1b467b026a5a9ce24d892b160e5956
Ammola <ooammolaoo@gmail.com>
2021-02-23 16:33:16 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import { Switch, Router, Route } from 'react-router-dom'
+import logo from './Mylogo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import Details from './components/Details';
+import Cart from './components/Cart/Cart';
+import Default from './components/Default';
+import ProductList from './components/ProductList';
+import Product from './components/Product';
+import Model from './components/Model';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+     <Navbar />
+     <Switch>
+       <Route exact path="/" component={ProductList}/>
+       <Route path="/details" component={Details}/>
+       <Route path="/cart" component={Cart}/>
+       <Route component={Default}/>
+     </Switch>
+     <Model />
+   </>
   );
 }
 
