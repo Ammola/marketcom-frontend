fb0b57b76873a8d522b1b3885cd6b0bc9b92b73e
Ammola <ooammolaoo@gmail.com>
2021-03-03 14:26:32 +0300

    make payment available to registered usesrs only

--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -4,47 +4,50 @@ import PayPalButton from './PayPalButton'
 
 
 
-export default function CartTotals({value, history}) {
-    const{cartSubTotal,cartTax,cartTotal,clearCart,cartTotalUSD,addOrder,addProductsToOrder} = value
+export default function CartTotals({ value, history }) {
+    const { cartSubTotal, cartTax, cartTotal, clearCart, cartTotalUSD, addOrder, addProductsToOrder } = value
     //const totalUSD = parseInt(cartTotal) * 0.27
-    console.log("cartTotalUSD: "+cartTotalUSD);
+    console.log("cartTotalUSD: " + cartTotalUSD);
     useEffect(() => {
         value.addTotals()
-      },[]);
+    }, []);
     return (
         <>
-        <div className="container">
-            <div className="row">
-                <div className="col-10 mt-2 ml-sm-5 ml-auto col-sm-8 text-capitaliza text-right">
-                    <Link to="/">
-                        <button className="btn btn-outline-danger text-uppercase mb-3 px-5" type="button" onClick={() => clearCart()}>
-                            clear cart
+            <div className="container">
+                <div className="row">
+                    <div className="col-10 mt-2 ml-sm-5 ml-auto col-sm-8 text-capitaliza text-right">
+                        <Link to="/">
+                            <button className="btn btn-outline-danger text-uppercase mb-3 px-5" type="button" onClick={() => clearCart()}>
+                                clear cart
 
                         </button>
-                    </Link>
-                    <h5>
-                        <span className="text-title">
-                            subtotal :
+                        </Link>
+                        <h5>
+                            <span className="text-title">
+                                subtotal :
                         </span>
-                        <strong>SR {cartSubTotal}</strong>
-                    </h5>
-                    <h5>
-                        <span className="text-title">
-                            tax :
+                            <strong>SR {cartSubTotal}</strong>
+                        </h5>
+                        <h5>
+                            <span className="text-title">
+                                tax :
                         </span>
-                        <strong>SR {cartTax}</strong>
-                    </h5>
-                    <h5>
-                        <span className="text-title">
-                            total :
+                            <strong>SR {cartTax}</strong>
+                        </h5>
+                        <h5>
+                            <span className="text-title">
+                                total :
                         </span>
-                        <strong>SR {cartTotal}</strong>
-                    </h5>
-                    <PayPalButton total={cartTotal} clearCart={clearCart} history={history} totalUSD = {cartTotalUSD} addOrder={addOrder}  addProductsToOrder={addProductsToOrder}/>
+                            <strong>SR {cartTotal}</strong>
+                        </h5>
+                        {!(localStorage.getItem('token')) ? <Link to="/register"><h3>
+                            <span className="text-title">please join us to complete payment</span></h3></Link>
+                        : <PayPalButton total={cartTotal} clearCart={clearCart} history={history} totalUSD={cartTotalUSD} addOrder={addOrder} addProductsToOrder={addProductsToOrder} />
+                        }
 
+                    </div>
                 </div>
             </div>
-        </div>
         </>
     )
 }
